20c6fec1e1e5f051069ceef2da0c49b83fea644a
Pascal Berndt <drook207@gmail.com>
2025-10-18 14:02:07 +0200

    Add missing include path

--- src/Storage/ListResourceMongoDBStorageFactory.h
+++ src/Storage/ListResourceMongoDBStorageFactory.h
@@ -1,6 +1,7 @@
 #ifndef LISTRESOURCEMONGODBSTORAGEFACTORY_H
 #define LISTRESOURCEMONGODBSTORAGEFACTORY_H
 
+#include "../Server/Resources/ListResource/IListResourceStorageFactory.h"
 
 class ListResourceMongoDBStorageFactory : public IListResourceStorageFactory
 {
